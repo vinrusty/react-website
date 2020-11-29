a57c0b1ef71e50e05ed322478565af56a1bcc3b7
Vineeth <vineethhr2002@gmail.com>
2020-11-29 11:00:24 +0530

    deploying my website

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Diwali from '../images/diwali.png'
+import Potter from '../images/potter.jpg'
+import Vinci from '../images/da vinci.jpg'
+import Symbols from '../images/symbols.jpg'
+import Potter2 from '../images/chamber.jpg'
 import Button from '../components/Button';
 import './home.css';
 const home=()=>{
     return(
         <>
         <div className='container pattern-checks-xl'>
-            <img className='logo' src='/images/diwali.png' alt='diwali'/>
+            <img className='logo' src={Diwali} alt='diwali'/>
             <div>
             <h1 className='heading'>Enjoy this Diwali by reading amazing stories that make you go wow!</h1>
             <Button/>
@@ -30,10 +35,10 @@ const home=()=>{
         <div className='main-content'>
             <h1 className='main-heading'>Explore a wide variety of books</h1>
             <div className='books pattern-grid-md'>
-                <img className='book-image' src='/images/potter.jpg' alt='Harry Potter and the cursed child' />
-                <img className='book-image' src='/images/da vinci.jpg' alt='Dan Brown The Da Vinci code' />
-                <img className='book-image' src='/images/symbols.jpg' alt='Dan Brown The Lost Symbol' />
-                <img className='book-image' src='/images/chamber.jpg' alt='Harry Potter and the chamber of secrets' />
+                <img className='book-image' src={Potter} alt='Harry Potter and the cursed child' />
+                <img className='book-image' src={Vinci} alt='Dan Brown The Da Vinci code' />
+                <img className='book-image' src={Symbols} alt='Dan Brown The Lost Symbol' />
+                <img className='book-image' src={Potter2} alt='Harry Potter and the chamber of secrets' />
             </div>
         </div>
         </>
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import joe from '../images/joe rogan.jpg'
+import conan from '../images/conan.jpg'
+import psycho from '../images/psycho.jpg'
+import mtbm from '../images/mtbm.jpeg'
+import folklore from '../images/folklore.jpg'
+import bts from '../images/bts.jpg'
+import shiva from '../images/shiva.png'
+import san from '../images/san.jpg'
+import girl from '../images/girl.jpg'
+import onepunch from '../images/one-punch.jpg'
+import dragon from '../images/dragon.jpg'
+import deathnote from '../images/death-note.jpg'
 import './services.css';
 
 const services=()=>{
@@ -9,9 +21,9 @@ const services=()=>{
             <div className='start'>
                 <h3 className='trending'>Trending <i class="fas fa-fire"></i></h3>
                 <div className='trend-images'>
-                    <img src='/images/joe rogan.jpg' alt="Joe Rogan's Podcast" className='trending-images'/>
-                    <img src='/images/conan.jpg' alt="Conan O'briens CONAN" className='trending-images'/>
-                    <img src='/images/psycho.jpg' alt="Psycho-Pass 3" className='trending-images'/>
+                    <img src={joe} alt="Joe Rogan's Podcast" className='trending-images'/>
+                    <img src={conan} alt="Conan O'briens CONAN" className='trending-images'/>
+                    <img src={psycho} alt="Psycho-Pass 3" className='trending-images'/>
                 </div>
             </div>
             <h1 className='head-start'>Make your holidays great again!</h1>
@@ -19,25 +31,25 @@ const services=()=>{
         <div className='music'>
             <h3 className='head-start'>Music <i class="fas fa-music"></i></h3>
                 <div className='music-images'>
-                    <img src='/images/mtbm.jpeg' alt="" className='m-image'/>
-                    <img src='/images/folklore.jpg' alt="" className='m-image'/>
-                    <img src='/images/bts.jpg' alt="" className='m-image'/>
+                    <img src={mtbm} alt="" className='m-image'/>
+                    <img src={folklore} alt="" className='m-image'/>
+                    <img src={bts} alt="" className='m-image'/>
                 </div>
         </div>
         <div className='art'>
             <h3 className='head-start'>Art <i class="fas fa-paint-brush"></i></h3>
                 <div className='art-images'>
-                    <img src='/images/shiva.png' alt="" className='a-image'/>
-                    <img src='/images/san.jpg' alt="" className='a-image'/>
-                    <img src='/images/girl.jpg' alt="" className='a-image'/>
+                    <img src={shiva} alt="" className='a-image'/>
+                    <img src={san} alt="" className='a-image'/>
+                    <img src={girl} alt="" className='a-image'/>
                 </div>
         </div>
         <div className='anime'>
             <h3 className='head-start'>Anime <i class="fas fa-tv"></i></h3>
                 <div className='anime-images'>
-                    <img src='/images/one-punch.jpg' alt="" className='an-image'/>
-                    <img src='/images/dragon.jpg' alt="" className='an-image'/>
-                    <img src='/images/death-note.jpg' alt="" className='an-image'/>
+                    <img src={onepunch} alt="" className='an-image'/>
+                    <img src={dragon} alt="" className='an-image'/>
+                    <img src={deathnote} alt="" className='an-image'/>
                 </div>
         </div>
         </>
